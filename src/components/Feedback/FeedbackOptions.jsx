9e41bff52dafd5b437c594aa6d23253009d09019
Denys <denys.drhn@gmail.com>
2023-04-15 14:21:05 +0300

    create FeadbackOptions.jsx

---
+++ src/components/Feedback/FeedbackOptions.jsx
@@ -0,0 +1,15 @@
+const FeedbackOptions = ({ options, onLeaveFeedback }) => (
+  <>
+    {options.map(option => (
+      <button
+        key={option}
+        type="button"
+        onClick={() => onLeaveFeedback(option)}
+      >
+        {option}
+      </button>
+    ))}
+  </>
+);
+
+export default FeedbackOptions;
