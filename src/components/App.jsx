310d985b878a762f25d990805a90564e7eefbe30
Denys <denys.drhn@gmail.com>
2023-04-17 13:43:11 +0300

    Update App.jsx
    remove propTypes
    add methods countTotalFeedback & countPositiveFeedbackPercentage
    
    rewrite options
    add render by condition on Statistics section

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,17 +1,10 @@
 import React, { Component } from 'react';
-import PropTypes from 'prop-types';
 import Section from './Section/Section';
 import FeedbackOptions from './FeedbackOptions/FeedbackOptions';
 import Statistics from './Statistics/Statistics';
+import Notification from './Notification/Notification';
 
 export class App extends Component {
-  static propTypes = {
-    good: PropTypes.number,
-    neutral: PropTypes.number,
-    bad: PropTypes.number,
-    onLeaveFeedback: PropTypes.func,
-  };
-
   state = {
     good: 0,
     neutral: 0,
@@ -22,28 +15,39 @@ export class App extends Component {
     this.setState(prevState => ({ [feedback]: prevState[feedback] + 1 }));
   };
 
+  countTotalFeedback = () => {
+    return this.state.good + this.state.neutral + this.state.bad;
+  };
+
+  countPositiveFeedbackPercentage = () => {
+    const total = this.state.good + this.state.neutral + this.state.bad;
+    return Math.round((this.state.good / total) * 100);
+  };
+
   render() {
     const { good, neutral, bad } = this.state;
-    const total = this.state.good + this.state.neutral + this.state.bad;
-    const positivePercentage = Math.round((good / total) * 100);
 
     return (
       <>
         <Section title="Please leave feedback">
           <FeedbackOptions
-            options={['good', 'neutral', 'bad']}
+            options={Object.keys(this.state)}
             onLeaveFeedback={this.onLeaveFeedback}
           />
         </Section>
 
         <Section title="Statistics">
-          <Statistics
-            good={good}
-            neutral={neutral}
-            bad={bad}
-            total={total}
-            positivePercentage={positivePercentage}
-          />
+          {this.countTotalFeedback() === 0 ? (
+            <Notification message="There is no feedback" />
+          ) : (
+            <Statistics
+              good={good}
+              neutral={neutral}
+              bad={bad}
+              total={this.countTotalFeedback}
+              positivePercentage={this.countPositiveFeedbackPercentage}
+            />
+          )}
         </Section>
       </>
     );
