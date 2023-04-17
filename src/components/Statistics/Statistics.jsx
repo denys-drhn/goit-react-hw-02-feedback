ebb5961f8908e3462d56000f4a5164a7c64ee65f
Denys <denys.drhn@gmail.com>
2023-04-17 13:40:47 +0300

    moved the component Notification, remove "if"
    moved the component Notification
    remove render  by condition to App
    fixed  props

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,33 +1,24 @@
 import PropTypes from 'prop-types';
-import Notification from './Notification';
 import css from './Statistics.module.css';
 
 const Statistics = ({ good, neutral, bad, total, positivePercentage }) => {
-  if (total <= 0) {
-    return <Notification message="There is no feedback" />;
-  } else {
-    return (
-      <>
-        <p className={css.item}>Good: {good}</p>
-        <p className={css.item}>Neutral: {neutral}</p>
-        <p className={css.item}>Bad: {bad}</p>
-        <p className={css.item}>Total: {total}</p>
-        {positivePercentage <= 0 ? (
-          <p className={css.item}>Positive feedback: 0%</p>
-        ) : (
-          <p className={css.item}>Positive feedback: {positivePercentage}%</p>
-        )}
-      </>
-    );
-  }
+  return (
+    <>
+      <p className={css.item}>Good: {good}</p>
+      <p className={css.item}>Neutral: {neutral}</p>
+      <p className={css.item}>Bad: {bad}</p>
+      <p className={css.item}>Total: {total()}</p>
+      <p className={css.item}>Positive feedback: {positivePercentage()}%</p>
+    </>
+  );
 };
 
 Statistics.propTypes = {
   good: PropTypes.number.isRequired,
   neutral: PropTypes.number.isRequired,
   bad: PropTypes.number.isRequired,
-  total: PropTypes.number.isRequired,
-  positivePercentage: PropTypes.number.isRequired,
+  total: PropTypes.func.isRequired,
+  positivePercentage: PropTypes.func.isRequired,
 };
 
 export default Statistics;
